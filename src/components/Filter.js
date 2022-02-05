f69904e1c3e36368e1a48fbd487d3f544952fc67
JonCabot <jfc0053@gmail.com>
2022-02-05 16:07:12 -0600

    completed assignment

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,30 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ search, onCategoryChange, onSearchChange }) { 
+  //take in current State of search to render to page (search)
+  //take in search setter (onSearchChange), so that when it fires, the State of search can be reset 
+  //take in category setter (onCategoryChange), so that onChange, the category State gets set
+  
+  function handleSearchChange(event) {
+    onSearchChange(event.target.value)
+  }
+
+  //value = current State of search
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+        type="text" 
+        name="search" 
+        placeholder="Search..." 
+        value={search}
+        onChange={handleSearchChange}
+      />
+      
       <select name="filter" onChange={onCategoryChange}>
-        <option value="All">Filter by category</option>
-        <option value="Produce">Produce</option>
-        <option value="Dairy">Dairy</option>
-        <option value="Dessert">Dessert</option>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
       </select>
     </div>
   );
